1a5e2bc4489d04f7126f6af9656398ff285f50fa
Juan Sebastian Marin Serna <sebasagosto2003@gmail.com>
2024-11-07 23:47:24 -0500

    Recorrido Aestrella completado

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import tkinter as tk
-from PIL import Image, ImageTk
-from collections import deque
 
-from dls_kermit import dls_move_kermit, dls, try_move
 from gui_elements import create_grid, load_images, resize_image, add_images
-from animated_paths import animate_piggy_path, animate_kermit_path
-
+from dls_kermit import dls_move_kermit, dls, try_move
+from bfs_piggy import check_piggy_movement, stop_piggy_animation, bfs_move_piggy, reconstruct_path
+from aestrella_piggy import a_star, reconstruct_path
+from animated_paths import astar_animate_piggy_path, animate_kermit_path
 
 class GameEnvironmentGUI:
     def __init__(self, root):
@@ -26,14 +25,16 @@ class GameEnvironmentGUI:
         self.create_grid(4, 5)
 
         # Posición inicial de Kermit, Elmo y Piggy
-        self.kermit_pos = [0, 4]  # Posición inicial de Kermit
+        self.kermit_pos = [3, 2]  # Posición inicial de Kermit
         self.elmo_pos = [2, 0]     # Posición de Elmo
-        self.wall_positions = [[3, 1], [2, 1], [1, 2], [2, 2]]  # Posiciones de varios muros
-        self.piggy_pos = [2, 3]    # Posición inicial de Piggy
+        self.wall_positions = [[3, 1], [2, 1], [0, 2], [0, 3]]  # Posiciones de varios muros
+        self.piggy_pos = [0, 4]    # Posición inicial de Piggy
 
+        self.current_kermit_pos = self.kermit_pos
+        self.current_piggy_pos = self.piggy_pos
         # Variables para controlar la animación de Piggy
-        self.piggy_animating = False
-        self.piggy_animation_step = 0
+        #self.piggy_animating = False
+        #self.piggy_animation_step = 0
 
         # Insertar imágenes
         self.add_images()
@@ -43,61 +44,11 @@ class GameEnvironmentGUI:
         self.dls_move_kermit()
 
         # Iniciar la verificación constante de la posición de Kermit para Piggy
-        self.check_piggy_movement()
+        #self.check_piggy_movement()
 
-    
+        # Iniciar la búsqueda A* para mover a Piggy hacia Kermit
+        self.animate_piggy_path()
 
-    def check_piggy_movement(self):
-        """Revisa constantemente la posición de Kermit y mueve a Piggy si es necesario."""
-        if self.kermit_pos == self.piggy_pos:
-            self.stop_piggy_animation()  # Detener la animación actual de Piggy
-        self.bfs_move_piggy()
-        self.root.after(1000, self.check_piggy_movement)  # Revisar cada segundo
-
-    def stop_piggy_animation(self):
-        """Detiene la animación actual de Piggy."""
-        self.piggy_animating = False
-
-    def bfs_move_piggy(self):
-        """Inicia la búsqueda por amplitud (BFS) para mover a Piggy hacia Kermit."""
-        queue = deque([self.piggy_pos])  # Cola para BFS
-        visited = set()  # Conjunto de posiciones visitadas
-        parent = {tuple(self.piggy_pos): None}  # Para reconstruir el camino
-
-        while queue:
-            current_pos = queue.popleft()
-
-            # Si encontramos a Kermit, reconstruimos el camino y animamos
-            if current_pos == self.kermit_pos:
-                path = self.reconstruct_path(parent, current_pos)
-                self.animate_piggy_path(path)
-                return
-
-            # Marcar esta posición como visitada
-            visited.add(tuple(current_pos))
-
-            # Coordenadas de movimiento (arriba, abajo, izquierda, derecha)
-            moves = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-            for move in moves:
-                new_pos = [current_pos[0] + move[0], current_pos[1] + move[1]]
-
-                # Comprobar si la nueva posición está dentro de los límites
-                if 0 <= new_pos[0] < 4 and 0 <= new_pos[1] < 5:
-                    # Comprobar si la nueva posición no es un muro y no ha sido visitada
-                    if new_pos not in self.wall_positions and tuple(new_pos) not in visited:
-                        queue.append(new_pos)
-                        parent[tuple(new_pos)] = current_pos
-
-    def reconstruct_path(self, parent, end_pos):
-        """Reconstruye el camino desde el final hasta el inicio usando el diccionario parent."""
-        path = []
-        current = end_pos
-        while current is not None:
-            path.append(current)
-            current = parent.get(tuple(current))
-        path.reverse()  # Invertir el camino para que vaya desde el inicio hasta el final
-        return path
 
 #GUI Elements
 GameEnvironmentGUI.create_grid = create_grid
@@ -110,9 +61,20 @@ GameEnvironmentGUI.dls_move_kermit = dls_move_kermit
 GameEnvironmentGUI.dls = dls
 GameEnvironmentGUI.try_move = try_move
 
+#Breadth First Search PIGGY
+GameEnvironmentGUI.check_piggy_movement = check_piggy_movement
+GameEnvironmentGUI.stop_piggy_animation = stop_piggy_animation
+GameEnvironmentGUI.bfs_move_piggy = bfs_move_piggy
+GameEnvironmentGUI.reconstruct_path = reconstruct_path
+
+# A* Search PIGGY
+GameEnvironmentGUI.a_star = a_star
+GameEnvironmentGUI.reconstruct_path = reconstruct_path
+
 #Animated Paths
-GameEnvironmentGUI.animate_piggy_path = animate_piggy_path
+GameEnvironmentGUI.animate_piggy_path = astar_animate_piggy_path
 GameEnvironmentGUI.animate_kermit_path = animate_kermit_path
+
 
 def main():
     root = tk.Tk()
